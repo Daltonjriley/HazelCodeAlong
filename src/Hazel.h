62d5809962b2708648cec0c9756ec33f7f9c7c23
Dalton <daltonjriley@gmail.com>
2025-12-12 15:57:23 -0600

    Entry point

---
+++ src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For use with Hazel applications
+
+#include "Hazel/Application.h"
+
+// ----------- entry point ------------
+
+#include "Hazel/EntryPoint.h"
---
+++ src/Hazel/EntryPoint.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifdef HZ_PATFORM_WINDOWS
+
+extern Hazel::Application* Hazel::CreateApplication();
+
+int main(int argc, char** argv) {
+
+	auto app = Hazel::CreateApplication();
+	app->Run();
+	delete app;
+
+}
+
+#endif
+ 
