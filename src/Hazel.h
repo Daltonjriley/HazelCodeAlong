d1718416a469a385d72e5df78b657cdcd7a94114
Dalton <daltonjriley@gmail.com>
2025-12-14 10:39:57 -0600

    Added Logging

--- src/Hazel.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// For use with Hazel applications
-
-#include "Hazel/Application.h"
-
-// ----------- entry point ------------
-
-#include "Hazel/EntryPoint.h"
---
+++ src/Hazel/Hazel.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// For use with Hazel applications
+
+#include "Application.h"
+#include "Log.h"
+
+// ----------- entry point ------------
+
+#include "EntryPoint.h" 
